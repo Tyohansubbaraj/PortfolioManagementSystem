8c92437dcd3a3adaf8f2a2208f123c98629274b8
tyohansubbaraj <t.yohan.subbaraj@gmail.com>
2026-01-31 13:04:39 +0530

    spring project creation

---
+++ src/main/java/com/marketminds/portfoliomanagementsystem/PortfoliomanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.marketminds.portfoliomanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliomanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliomanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketminds/portfoliomanagementsystem/PortfoliomanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketminds.portfoliomanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliomanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
